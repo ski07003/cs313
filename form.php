170355ffef44f30e8023309b560b120ba38cafeb
ski07003 <ski07003@byui.edu>
2014-01-24 15:11:32 -0700

    Here comes some more good stuff. Starting to implement php structure with modules as headers navigations and footers.

---
+++ form.php
@@ -0,0 +1,78 @@
+<?php
+start_session();
+if(isset($_SESSION["views"]))
+{
+  $_SESSION['views'] = $_SESSION['views'] + 1;
+  header('/var/www/html/cs313/results.php');
+}
+else
+{
+  $_SESSION['views'] = 1;
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php $head = file_get_contents('/var/www/html/cs313/modules/head.html');
+    echo $head;
+    ?>
+  </head>
+
+  <body>
+    <div id="navigation">
+      <?php $nav = file_get_contents('/var/www/html/cs313/modules/navigation.html');
+      echo $nav;
+      ?>
+    </div>
+
+    <form name="input" action="action.php" method="get">
+      First Name: <input type="text" name="firstname"><br/>
+      Last Name: <input type="text" name="lastname"><br/>
+      If you could have any one of the following super powers, what would it be? <br/>
+      <input type="radio" name="power" value="Flight">Fly<br/>
+      <input type="radio" name="power" value="Invisibility">Invisible<br/>
+      <input type="radio" name="power" value="Teleportation">Teleportation<br/>
+      <input type="radio" name="power" value="Time Travel">Time Travel<br/>
+      <input type="radio" name="power" value="Super Speed">Super Speed<br/>
+      <input type="radio" name="power" value="Super Strength">Super Strength<br/>
+      
+      Which is best?<br/>
+      <input type="radio" name="food" value="Bacon Burger">Bacon Burger<br/>
+      <input type="radio" name="food" value="BLT">BLT<br/>
+      <input type="radio" name="food" value="Lasagna">Lasagna<br/>
+      <input type="radio" name="food" value="Chicken Caesar Salad">Chicken Caesar Salad<br/>
+      <input type="radio" name="food" value="Chicken Bacon Artichoke Pizza">Chicken Bacon Artichoke Pizza<br/>
+      <input type="radio" name="food" value="Frozen Yogurt">Frozen Yogurt<br/>
+
+      Which flavor is best?<br/>
+      <input type="radio" name="flavor" value="Red">Red<br/>
+      <input type="radio" name="flavor" value="Blue">Blue<br/>
+      <input type="radio" name="flavor" value="Green">Green<br/>
+      <input type="radio" name="flavor" value="Pink">Pink<br/>
+      <input type="radio" name="flavor" value="Yellow">Yellow<br/>
+      <input type="radio" name="flavor" value="Orange">Orange<br/>
+
+      Who would win in a fight, Gandalf or Dumbledore?<br/>
+      <input type="radio" name="fight" value="Dumbledore">Dumbledore<br/>
+      <input type="radio" name="fight" value="Gandalf">Gandalf<br/>
+
+
+      <input type="submit" value="Submit">
+    </form>
+
+    <?php
+    echo $_COOKIE["user"];
+    ?>
+
+
+  </body>
+
+  <footer>
+    <?php $foot = file_get_contents('/var/www/html/cs313/modules/footer.html');
+    echo $foot;
+    ?>
+  </footer>
+</html>
---
+++ index.php~
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php $head = file_get_contents('/var/www/html/cs313/modules/head.html');
+          echo $head;
+           ?>
+  </head>
+
+  <body>
+    <div id="navigation">
+      <?php $nav = file_get_contents('/var/www/html/cs313/modules/navigation.html');
+            echo $nav;
+    </div>
+  </body>
+
+  <footer>
+    <?php $foot = file_get_contents('/var/www/html/cs313/modules/footer.html');
+          echo $foot;
+  </footer>
+</html>
